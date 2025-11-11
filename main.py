37f0b68c1525740b3b629fd8c6547079294b899b
smartideasatendimento-debug <smartideas.atendimento@gmail.com>
2025-11-11 11:02:07 -0800

    Update tennis sport keys

--- main.py
+++ main.py
@@ -5,10 +5,37 @@ from tenacity import retry, stop_after_attempt, wait_exponential
 
 # -------------------- CONFIG BÁSICA --------------------
 SPORT_KEYS = [
-    "tennis_atp",
-    "tennis_wta",
-    "tennis_atp_challenger",
-    "tennis_wta_challenger",
+       "tennis_atp_aus_open_singles",
+    "tennis_atp_canadian_open",
+    "tennis_atp_china_open",
+    "tennis_atp_cincinnati_open",
+    "tennis_atp_dubai",
+    "tennis_atp_french_open",
+    "tennis_atp_indian_wells",
+    "tennis_atp_italian_open",
+    "tennis_atp_madrid_open",
+    "tennis_atp_miami_open",
+    "tennis_atp_monte_carlo_masters",
+    "tennis_atp_paris_masters",
+    "tennis_atp_qatar_open",
+    "tennis_atp_shanghai_masters",
+    "tennis_atp_us_open",
+    "tennis_atp_wimbledon",
+    "tennis_wta_aus_open_singles",
+    "tennis_wta_canadian_open",
+    "tennis_wta_china_open",
+    "tennis_wta_cincinnati_open",
+    "tennis_wta_dubai",
+    "tennis_wta_french_open",
+    "tennis_wta_indian_wells",
+    "tennis_wta_italian_open",
+    "tennis_wta_madrid_open",
+    "tennis_wta_miami_open",
+    "tennis_wta_qatar_open",
+    "tennis_wta_us_open",
+    "tennis_wta_wimbledon",
+    "tennis_wta_wuhan_open",
+
 ]
 REGIONS = ["eu", "uk", "us"]  # regiões de books na The Odds API
 MARKETS = ["h2h"]  # mercado head‑to‑head
